1bf3fe2776c22a5b6f50dee08bc2525d172cd440
fguiotte <florent.guiotte@irisa.fr>
2020-03-25 12:11:34 +0100

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sap',
-    version='0.1.5',
+    version='0.1.6',
     author='Florent Guiotte',
     author_email='florent.guiotte@irisa.fr',
     description='Simple Attribute Profiles',

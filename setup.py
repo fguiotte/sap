319005fa0f809ef745262e4c05fb3f2550e6da1e
fguiotte <florent.guiotte@irisa.fr>
2020-06-17 10:45:42 +0200

    Important fix on vectorize() when AP and not EAP

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sap',
-    version='0.2.2',
+    version='0.2.3',
     author='Florent Guiotte',
     author_email='florent.guiotte@irisa.fr',
     description='Simple Attribute Profiles',

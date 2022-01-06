a9d0ed87b10c891b31002cb14e2e3bbaf7e8cf83
Florent Guiotte <florent.guiotte@irisa.fr>
2022-01-06 16:10:36 +0100

    Change doc theme to RTD

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,7 @@
 #import os
 #import sys
 #sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -39,6 +40,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 napoleon_google_docstring = False
@@ -59,8 +61,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
